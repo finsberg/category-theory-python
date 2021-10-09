7d78cf6577c3c9a776cf906153b69033874053d5
Henrik Finsberg <henriknf@simula.no>
2021-10-09 21:17:31 +0200

    Add beginning of monoid

---
+++ monoid.py
@@ -0,0 +1,55 @@
+"""
+A moniod is a Set S equipped with a binary operation + : S x S -> S 
+such that the two following axioms holds
+
+1) + is associative: (a + b) + c = a + (b + c)
+2) There exists an identiry element e, such that a + e = e + a = a
+"""
+import typing
+import functools
+from abc import ABC, abstractstaticmethod, abstractmethod
+
+class Monoid(ABC):
+
+    abstractstaticmethod
+    def e():
+        pass
+
+    @abstractmethod
+    def __radd__(self, other: "Monoid"):
+        pass
+
+    @abstractmethod
+    def __add__(self, other: "Monoid"):
+        pass
+
+
+def squash(lst: typing.Iterable["Monoid"], cls: typing.Type):
+    return functools.reduce(lambda x, y : x + y, lst, cls.e())
+
+class CommutativeMonoid(Monoid):
+    def __radd__(self, other: typing.Any) -> typing.Any:
+        return self.__add__(other)
+
+class IntPlus(CommutativeMonoid):
+    def __init__(self, value: int):
+        self.value = value
+
+    @staticmethod
+    def e():
+        return 0
+
+    def __add__(self, other: int) -> int:
+        return self.value + other
+
+
+class IntProd(CommutativeMonoid):
+    def __init__(self, value: int):
+        self.value = value
+
+    @staticmethod
+    def e():
+        return 1
+
+    def __add__(self, other: int) -> int:
+        return self.value * other
---
+++ test_monoid.py
@@ -0,0 +1,13 @@
+import monoid
+import operator
+import functools
+
+def test_IntPlus():
+    values = [1, 2, 3]
+    value = monoid.squash((monoid.IntPlus(v) for v in values), monoid.IntPlus)
+    assert value == sum(values)
+
+def test_IntProd():
+    values = [1, 2, 3]
+    value = monoid.squash((monoid.IntProd(v) for v in values), monoid.IntProd)
+    assert value == functools.reduce(operator.mul, values)
